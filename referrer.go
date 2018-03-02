9b7d197e361ab991e72c2bcf98b7d95dbd26d13c
Tyler Mercier <tylermercier@gmail.com>
2018-03-02 11:04:59 -0500

    add marketing channel definitions

--- referrer.go
+++ referrer.go
@@ -1,5 +1,34 @@
 package goreferrer
 
+type Referrer struct {
+	Type       ReferrerType
+	Label      string
+	URL        string
+	Subdomain  string
+	Domain     string
+	Tld        string
+	Path       string
+	Query      string
+	GoogleType GoogleSearchType
+	Channel    MarketingChannel
+}
+
+func (r *Referrer) RegisteredDomain() string {
+	if r.Domain != "" && r.Tld != "" {
+		return r.Domain + "." + r.Tld
+	}
+
+	return ""
+}
+
+func (r *Referrer) Host() string {
+	if r.Subdomain != "" {
+		return r.Subdomain + "." + r.RegisteredDomain()
+	}
+
+	return r.RegisteredDomain()
+}
+
 type ReferrerType int
 
 const (
@@ -28,34 +57,6 @@ func (r ReferrerType) String() string {
 	}
 }
 
-type Referrer struct {
-	Type       ReferrerType
-	Label      string
-	URL        string
-	Subdomain  string
-	Domain     string
-	Tld        string
-	Path       string
-	Query      string
-	GoogleType GoogleSearchType
-}
-
-func (r *Referrer) RegisteredDomain() string {
-	if r.Domain != "" && r.Tld != "" {
-		return r.Domain + "." + r.Tld
-	}
-
-	return ""
-}
-
-func (r *Referrer) Host() string {
-	if r.Subdomain != "" {
-		return r.Subdomain + "." + r.RegisteredDomain()
-	}
-
-	return r.RegisteredDomain()
-}
-
 type GoogleSearchType int
 
 const (
@@ -72,5 +73,41 @@ func (g GoogleSearchType) String() string {
 		return "organic google search"
 	case Adwords:
 		return "google adwords referrer"
+	}
+}
+
+type MarketingChannel int
+
+const (
+	DirectChannel MarketingChannel = iota
+	OrganicSearchChannel
+	PaidSearchChannel
+	SocialChannel
+	EmailChannel
+	OrganicReferralChannel
+	DisplayChannel
+	PaidReferralChannel
+)
+
+func (r MarketingChannel) String() string {
+	switch r {
+	default:
+		return "unknown"
+	case DirectChannel:
+		return "direct"
+	case OrganicSearchChannel:
+		return "organic_search"
+	case PaidSearchChannel:
+		return "paid_search"
+	case SocialChannel:
+		return "social"
+	case EmailChannel:
+		return "email"
+	case OrganicReferralChannel:
+		return "organic_referral"
+	case DisplayChannel:
+		return "display"
+	case PaidReferralChannel:
+		return "paid_referral"
 	}
 }
